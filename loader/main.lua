5fc60c66b63c671b06946ae81dc8e6a39272d05e
Lost-dev7 <147116158+Lost-dev7@users.noreply.github.com>
2024-02-06 19:50:03 -0300

    update loader/main.lua

--- loader/main.lua
+++ loader/main.lua
@@ -1,3 +1,14 @@
 function AutoFarm()
-  while AutoFarm == true do
+  while _G.AutoFarm == true do task.wait(0.1)
+    local args = {
+    [1] = "collectOrb",
+    [2] = "Red Orb",
+    [3] = "City"
+}
+
+game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+  end
 end
+
+AutoFarm = true
+AutoFarm()
